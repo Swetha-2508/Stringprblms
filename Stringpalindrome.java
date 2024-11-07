5fb32069975e35b3a9da4f8585acf4fc9c05a81a
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2024-11-08 00:05:53 +0530

    Create Stringpalindrome.java

---
+++ Stringpalindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Stringpalindrome {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        boolean isPalindrome=true;
+        System.out.println("Enter the text");
+        for(int i=0;i<A.length()/2;i++)
+        {
+            if(A.charAt(i)!=A.charAt(A.length()-1-i))
+            {
+                isPalindrome=false;
+                break;
+            }
+        }
+        if(isPalindrome==true)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
