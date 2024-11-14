265f6171712cc63a81a8e063290f66682640d883
Swetha <163889342+Swetha-2508@users.noreply.github.com>
2024-11-14 21:17:27 +0530

    Update Stringpalindrome.java

--- Stringpalindrome.java
+++ Stringpalindrome.java
@@ -4,9 +4,9 @@ public class Stringpalindrome {
     public static void main(String[] args) {
         
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the text");
         String A=sc.next();
         boolean isPalindrome=true;
-        System.out.println("Enter the text");
         for(int i=0;i<A.length()/2;i++)
         {
             if(A.charAt(i)!=A.charAt(A.length()-1-i))
